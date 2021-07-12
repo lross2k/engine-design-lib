11f0dc1ef39cf24fb740af442a9d61d559f5f05d
JozueMR01 <80725565+JozueMR01@users.noreply.github.com>
2021-07-12 11:12:23 -0600

    Rendimientos

---
+++ src/Rendimientos/Rendimientos/comp_tiempo.h
@@ -0,0 +1,10 @@
+#ifndef COMP_TIEMPO
+#define COMP_TIEMPO
+
+//Constructores
+float empuje_prom_tiempo(float list[]);
+float impulso_tot(float list[]);
+float impulso_esp(float list[]);
+float delta_V(float list[]);
+
+#endif COMP_TIEMPO
